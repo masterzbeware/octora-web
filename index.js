234f00f4ad9bedd5f57340f7aae4d0b7b266c44c
masterzbeware <emailmu@contoh.com>
2026-04-14 16:50:37 +0700

    hapus semua file

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.end("OK");
-}).listen(PORT, () => {
-  console.log("Web running on", PORT);
-});
